a3b949ec284b5f4a9ec7953804e656144281bd08
David Ukelere <david.ukelere291@gmail.com>
2021-03-08 15:37:50 +0100

    Update browserlist

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import HeroImage from '../HeroImage/HeroImage'
 import Searchbar from '../SearchBar/Searchbar'
 import FourColGrid from '../FourColGrid/FourColGrid'
-import MovieThumb from '../MovieThumb/MovieThumb'
+/* import MovieThumb from '../MovieThumb/MovieThumb' */
 import LoadMoreBtn from '../LoadMoreBtn/LoadMoreBtn'
 import Spinner from '../Spinner/Spinner'
 import './Home.css'
