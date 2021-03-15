6fe0998042179e4424c347fef0b5e74a45506528
David Ukelere <david.ukelere291@gmail.com>
2021-03-15 11:30:52 +0100

    Set movie scaffold

---
+++ src/components/Actor/Actor.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { IMAGE_BASE_URL } from '../config'
+import './Actor.css'
+
+export default function Actor() {
+    return (
+        <div>
+
+        </div>
+    )
+}
--- src/components/Movie/Movie.js
+++ src/components/Movie/Movie.js
@@ -1,10 +1,22 @@
 import React, { Component } from 'react'
+import { API_URL, API_KEY } from '../config.js';
+import Navigation from '../Navigation/Navigation'
+import MovieInfo from '../MovieInfo/MovieInfo'
+import MovieInfoBar from '../MovieInfoBar/MovieInfoBar'
+import FourColGrid from '../FourColGrid/FourColGrid'
+/* import Actor from '../Actor/Actor' */
+import Spinner from '../Spinner/Spinner'
+import './Movie.css'
 
 export default class Movie extends Component {
     render() {
         return (
-            <div>
-
+            <div className="rmdb-movie">
+                <Navigation />
+                <MovieInfo />
+                <MovieInfoBar />
+                <FourColGrid />
+                <Spinner />
             </div>
         )
     }
