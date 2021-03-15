a5a1a458ca4c1b66095c59aaf2a50a7ede0fa7f6
David Ukelere <david.ukelere291@gmail.com>
2021-03-15 12:12:16 +0100

    Setup fetch in movie

--- src/components/Movie/Movie.js
+++ src/components/Movie/Movie.js
@@ -9,13 +9,46 @@ import Spinner from '../Spinner/Spinner'
 import './Movie.css'
 
 export default class Movie extends Component {
+    state = {
+        movie: null,
+        actors: null,
+        directors: [],
+        loading: false
+    }
+
+    componentDidMount() {
+        this.setState({ loading: true })
+        const endpoint = `${API_URL}movie/${this.props.match.params.id}?api_key=${API_KEY}%language=en-US`
+        this.fetchItems(endpoint)
+    }
+    fetchItems(endpoint) {
+        fetch(endpoint)
+            .then(result => result.json())
+            .then(result => {
+                if (result.status_code) this.setState({ loading: false })
+                else {
+                    this.setState({ movie: result }, () => {
+                        const endpoint = `${API_URL}movie/${this.props.match.params.movieId}/credits?api-key=${API_KEY}`
+                        fetch(endpoint)
+                            .then(result => result.json())
+                            .then(result => {
+                                const directors = result.crew.filter((member) => member.job === "Director");
+                                this.setState({
+                                    actors: result.cast,
+                                    directors,
+                                    loading: false
+                                })
+                            })
+                    })
+                }
+            })
+    }
     render() {
         return (
             <div className="rmdb-movie">
                 <Navigation />
                 <MovieInfo />
                 <MovieInfoBar />
-                <FourColGrid />
                 <Spinner />
             </div>
         )
